70c2f7a16c4ccc8f80e4c21f71d2d95e13586dc9
Admin <Admin@LENOVO>
2023-05-15 18:14:15 +0600

    Добавьте файлы проекта.

---
+++ HomeworkMVCPageStatistics/HttpRequestExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace HomeworkMVCPageStatistics
+{
+    public static class HttpRequestExtension
+    {
+        public static string? BaseUrl(this HttpRequest req)
+        {
+            if (req == null) return null;
+            var uriBuilder = new UriBuilder(req.Scheme, req.Host.Host, req.Host.Port ?? -1);
+            if (uriBuilder.Uri.IsDefaultPort)
+            {
+                uriBuilder.Port = -1;
+            }
+
+            return uriBuilder.Uri.AbsoluteUri;
+        }
+    }
+}
---
+++ HomeworkMVCPageStatistics/PathFinder.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System.Runtime.CompilerServices;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text;
+
+namespace HomeworkMVCPageStatistics
+{
+    public class PathFinder
+    {
+        private readonly RequestDelegate _requestDelegate;
+
+        public PathFinder(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate; 
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            using (FileStream fstream = new FileStream("path.txt", FileMode.OpenOrCreate))
+            {
+                //string url = HttpContext.Current.Request.Url.AbsoluteUri;
+                // http://localhost:1302/TESTERS/Default6.aspx
+
+                //string path = HttpContext.Current.Request.Url.AbsolutePath;
+                // /TESTERS/Default6.aspx
+
+                //string host = HttpContext.Current.Request.Url.Host;
+                // localhost
+
+                string path = Path.Combine(context.Request.Scheme, context.Request.Protocol, context.Request.Path);
+
+                byte[] buffer = Encoding.Default.GetBytes(path);
+
+                fstream.Write(buffer);
+            }
+
+            return _requestDelegate.Invoke(context);    
+        }
+    }
+
+    public static class PathFinderExtension
+    {
+        public static IApplicationBuilder mw(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<PathFinder>();
+        }
+    }
+}
